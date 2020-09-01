115513806a26d2dfaa0a68ee316affcf58109d9c
nerdeaux <vusi.sizani@gmail.com>
2020-09-01 14:53:20 +0200

    routing update

--- src/App.js
+++ src/App.js
@@ -1,75 +1,59 @@
-import './App.css';
-//import Products from './Products';
-//import { Button } from 'react-bootstrap';
-//import Rating from './Rating';
-//import JumboTronComponent from './JumboTronComponent';
-//import UserForm from './UserForm';
-import GitHub from './GitHub';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import React, { Component } from 'react';
-import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import GitHub from './GitHub';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Nav, Navbar } from 'react-bootstrap';
+import GitHubUser from './GitHubUser';
 
-function App() {
+class App extends Component {       
+  render() {        
     return (
-        //<div className="App">
-        //<Products />
-        //<JumboTronComponent body='...'/>
-        //<UserForm /> 
-        //<GitHub/>
-        //</div>
-        <div>
-            <Header />
-        </div>
+      <div>
+        <Header />
+      </div>
     );
+  }
 }
 export default App;
 
 class Header extends Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <div>
-                    <Navbar bg="light" expand="lg">
-                        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="mr-auto">
-                                <Nav.Link href="/">Home</Nav.Link>
-                                <Nav.Link href="/github">GitHub</Nav.Link>
-                                <Nav.Link href="/login">Login</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Navbar>
-                    <Switch>
-                        <Route path="/github" component={GitHub} />
-                        <Route exact path="/" component={Home} />
-                        <Route path="/*" component={NotFound} />
-                        <Route path="/login" component={Login} />
-                    </Switch>
-                </div>
-            </BrowserRouter>
-        )
-    }
+  render(){
+    return (
+        <BrowserRouter>
+          <div>            
+            <Navbar bg="light" expand="lg">
+              <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link href="/">Home</Nav.Link>
+                  <Nav.Link href="/github">GitHub</Nav.Link>                  
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+            <Switch> 
+              <Route path="/github/user/:login/:id" component={GitHubUser} />                                                                
+              <Route path="/github" component={GitHub} />              
+              <Route exact path="/" component={Home} />             
+              <Route path="/*" component={NotFound} />                       
+            </Switch>   
+          </div>  
+        </BrowserRouter>              
+    )
+  }
 }
 
 class Home extends Component {
-    render() {
-        return (
-            <div>
-                Home
-            </div>
-        )
-    }
+  render(){
+    return (
+      <div>
+        Home        
+      </div>
+    )
+  }
 }
 
 class NotFound extends Component {
-    render() {
-        return <div>Not Found</div>
-    }
-}
-
-class Login extends Component {
-    render() {
-        return (<div>Login Page</div>)
-    }
+  render(){
+    return <div>Not Found</div>
+  }
 }
--- src/GitHub.js
+++ src/GitHub.js
@@ -1,52 +1,45 @@
 import React, { Component } from 'react';
-import axios from 'axios'; // npm install axios;
+import axios from 'axios'; 
 import ReactLoading from 'react-loading';
-import { Media, Form, FormGroup, FormControl, Button } from 'react-bootstrap';
+import { Media, Form, Button, Nav } from 'react-bootstrap';
 
-class GitHub extends Component {
-    constructor() {
-        super();
+class GitHub extends Component {    
+    
+    constructor(){
+        super();  
         this.state = {
             data: [],
-            searchTerm: '',
-            isLoading: false
-        };
+            searchTerm:'',
+            isLoading : false            
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        }; 
+
+        this.handleChange = this.handleChange.bind(this);  
+        this.handleSubmit = this.handleSubmit.bind(this);  
+
     }
 
-    componentDidMount() {
+    componentDidMount(){
         //this.getGitHubData('greg');
-    }
+    }	
 
-    getGitHubData(_searchTerm) {
-        axios.get("https://api.github.com/search/users?q=" + _searchTerm)
-            .then(res => {
+
+    getGitHubData(_searchTerm){                
+        axios.get("https://api.github.com/search/users?q="+_searchTerm)
+            .then(res => {   
                 this.setState({
-                    isLoading: false,
-                    data: res.data.items
-                })
-                console.log(res.data.items);
-            });
-    }
+                    isLoading : false, 
+                    data: res.data.items                                  
+                })  
+                             
+                console.log(res.data.items); 
+            });             
+    }          
 
-    handleSubmit(e) {
-        e.preventDefault();
-        this.setState({
-            isLoading: true
-        })
-        this.getGitHubData(this.state.searchTerm);
-    }
-
-    handleChange(e) {
-        this.setState({ searchTerm: e.target.value });
-    }
-
-    render() {
-        const listUsers = this.state.data.map((user) =>
+    render() { 
+        const listUsers = this.state.data.map(user =>             
             <Media key={user.id}>
-                <a href={user.html_url}>
+                <Nav.Link href={`/github/user/${user.login}/${user.id}`}> 
                     <img
                         width={64}
                         height={64}
@@ -54,39 +47,52 @@ class GitHub extends Component {
                         src={user.avatar_url}
                         alt="Generic placeholder"
                     />
-                </a>
+                </Nav.Link>
                 <Media.Body>
                     <h5>Login: {user.login}</h5>
-                    <p>Id: {user.id}</p>
+                    <p>Id: { user.id }</p>
                 </Media.Body>
-            </Media>
-        );
+            </Media>                     
+        );        
+
         return (
-            <div>
-                <Form inline onSubmit={this.handleSubmit}>
-                    <FormGroup controlId="formInlineName">
-                        <FormControl
-                            type="text"
-                            value={this.state.searchTerm}
-                            placeholder="Enter Search Term"
-                            onChange={this.handleChange}
-                        />
-                    </FormGroup>
-                    {' '}
-                    <Button type="submit">
-                        Search
-                    </Button>
-                </Form>
+          <div>
+            <Form inline onSubmit={this.handleSubmit}>
+                <Form.Group controlId="formInlineName">                                    
+                    <Form.Control 
+                        type="text" 
+                        value={this.state.searchTerm} 
+                        placeholder="Enter Search Term" 
+                        onChange={this.handleChange}
+                    />
+                </Form.Group>                            
+                {' '}
+                <Button type="submit">
+                    Search
+                </Button>
+            </Form>                 
 
-                <h3>GitHub Users Results</h3>
-
-                {this.state.isLoading &&
-                    <ReactLoading type="spinningBubbles" color="#444" />
-                }
-                
-                {listUsers}
-            </div>
+            <h3>GitHub Users Results</h3>
+            { this.state.isLoading &&                                             
+                <ReactLoading type="spinningBubbles" color="#444" /> 
+            }  
+            {listUsers}                                                                                            
+          </div>
         );
     }
+    
+    handleSubmit(e) {
+        e.preventDefault();    
+        this.setState({
+            isLoading : true          
+        })                
+        this.getGitHubData(this.state.searchTerm);        
+    }
+
+    handleChange(e) {
+        this.setState({ searchTerm: e.target.value });        
+    }
+
+    
 }
 export default GitHub;
